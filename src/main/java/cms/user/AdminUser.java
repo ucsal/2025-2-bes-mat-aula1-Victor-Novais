13f48f1935fa1b463c6d10bae67a25a9cba63e00
Victor-Novais <victornovais1337@gmail.com>
2025-08-20 21:48:31 -0300

    fix: Corrige erro de sintaxe no construtor de AdminUser

--- src/main/java/cms/user/AdminUser.java
+++ src/main/java/cms/user/AdminUser.java
@@ -4,7 +4,7 @@ public class AdminUser extends User {
 
 	public AdminUser(int id, String nome, String email, String password) {
 		super (id, nome, email, password);
-	}}
+	}
 
 	public void deleteContent(int contentId) {
 		/* exclusivo do admin */ }
