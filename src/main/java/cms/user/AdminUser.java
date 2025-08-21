28461d7560b63eac0878b374da01840450968dfe
Victor-Novais <victornovais1337@gmail.com>
2025-08-20 21:41:26 -0300

    refactor: Faz GuestUser herdar de User e corrige sintaxe no construtor de AdminUser

--- src/main/java/cms/user/AdminUser.java
+++ src/main/java/cms/user/AdminUser.java
@@ -3,7 +3,7 @@ package cms.user;
 public class AdminUser {
 
 	public AdminUser(int id, String nome, String email, String password) {
-		super (id, nome, email, password)
+		super (id, nome, email, password);
 	}}
 
 	public void deleteContent(int contentId) {
